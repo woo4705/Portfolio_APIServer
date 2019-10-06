069cda4bd6d5680e0e58da3d0ea0bb9a7fae0534
woo4705 <woo4705@naver.com>
2019-10-06 14:48:44 +0900

    API_Server코드

---
+++ APIServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetrisApiServer
+{
+    public class APIServer
+    {
+        static public void Init(ServerOptions options)
+        {
+            DBRedis.Init(options.RedisName, options.RedisAddress);
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TetrisApiServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Login")]
+    public class LoginController : Controller
+    {
+        [HttpPost]
+        public async Task<LoginRes> Process([FromBody] LoginReq request)
+        {
+            var loginResult = await DBMysql.LoginUser(request.UserID, request.UserPW);
+
+            if(loginResult == false)
+            {
+                return new LoginRes() { Result = -1 };
+            }
+
+            SetLoginTime(request.UserID);
+
+            var authToken = CreateAuthToken();
+            var result = await DBRedis.SetValue(request.UserID, authToken);
+
+            return new LoginRes() { Result = 0, AuthToken = authToken };
+
+        }
+
+        string CreateAuthToken()
+        {
+            string token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            return token;
+        }
+
+        async void SetLoginTime(string userID)
+        {
+            var tick = DateTime.Now.Ticks.ToString();
+        //    await DBMemcached.SetString(userID, tick);
+        }
+    }
+
+    public class LoginReq
+    {
+        public string UserID;
+        public string UserPW;
+    }
+
+    public class LoginRes
+    {
+        public short Result;
+        public string AuthToken;
+    }
+}
